0bf484246bf1e3aefff5109bc40a0df8f1455cef
Hoescu Radu <hoescu_radu@yaho.com>
2020-06-22 10:33:52 +0300

    video render

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,11 +2,12 @@ import React, {useState} from "react";
 
 const VideoItem = ({video}) => {
 
-    const [iframe, setIframe] = useState(<img src={video.snippet.thumbnails.default.url}
-                                              alt={video.snippet.description}/>);
+    const frame = <img src={video.snippet.thumbnails.default.url}
+                                              alt={video.snippet.description}/>;
 
+    const [iframe, setIframe] = useState('');
     const handleChange = () => {
-        setIframe(<iframe src={`https://www.youtube.com/embed/${video.id.videoId}?autoplay=1`} frameBorder="0"
+        setIframe(<iframe id="play" src={`https://www.youtube.com/embed/${video.id.videoId}?autoplay=1`} frameBorder="0"
                           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                           allowFullScreen/>)
     };
@@ -15,13 +16,14 @@ const VideoItem = ({video}) => {
         <>
             <div className="container">
                 <div id="image" onClick={handleChange}>
-                    {iframe}
+                    {frame}
                 </div>
                 <div id="title">
                     {video.snippet.title}
                 </div>
                 <button id="button" type="button" className="btn btn-warning">Save</button>
             </div>
+            <div className="render">{iframe}</div>
         </>
     );
 };
